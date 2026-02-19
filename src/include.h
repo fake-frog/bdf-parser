f1f44bf5d2790d281799058de5ebdce1bc1a6d69
fake-frog <afakefrog@gmail.com>
2026-02-18 21:13:58 -0700

    Initial commit

---
+++ src/include.h
@@ -0,0 +1,12 @@
+#ifndef INCLUDE_H
+#define INCLUDE_H
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <termios.h>
+#include <unistd.h>
+
+#endif
